bf9bc78394b716d027d5c0137935747f9328ea91
leozamboni <leonardonunes169@gmail.com>
2022-02-23 01:16:05 -0300

    add

---
+++ holyc-interpreter.js
@@ -0,0 +1,15 @@
+document.getElementById('textbox').addEventListener('keydown', function(e) {
+    if (e.key == 'Tab') {
+      e.preventDefault();
+      var start = this.selectionStart;
+      var end = this.selectionEnd;
+  
+      // set textarea value to: text before caret + tab + text after caret
+      this.value = this.value.substring(0, start) +
+        "\t" + this.value.substring(end);
+  
+      // put caret at right position again
+      this.selectionStart =
+        this.selectionEnd = start + 1;
+    }
+  });
