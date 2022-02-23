b7456b35ceb39648aead45b934e81c7dbbb4eaf6
leozamboni <leonardonunes169@gmail.com>
2022-02-23 16:37:51 -0300

    update

--- holyc-interpreter.js
+++ holyc-interpreter.js
@@ -1,10 +1,17 @@
 class Ast {
-	constructor(token, left, right, next) {
+  constructor(token, left, right, next) {
     this.token = token;
     this.next = next;
-		this.left = left;
-		this.right = right;
-	}
+    this.left = left;
+    this.right = right;
+  }
+}
+
+class ExpList {
+  constructor(ast, next) {
+    this.ast = ast;
+    this.next = next;
+  }
 }
 
 var tokenType = {
@@ -14,28 +21,112 @@ var tokenType = {
   sub: 4,
   div: 5,
   mul: 6,
+  semi: 7,
+  lbrace: 8,
+  rbrace: 9,
+  u0: 10,
+  i8: 11,
+  u8: 12,
+  i16: 13,
+  u16: 14,
+  i32: 15,
+  u32: 16,
+  i64: 17,
+  u64: 18,
+  f64: 19,
 };
 
 var is_alpha = (val) => {
-  if (val === " ") return false;
-  return /^[A-Z]$/i.test(val);
+  if (val === " " || val === "\n") return false;
+  return /^[A-Z0-9]$/i.test(val);
 };
 
 var is_digit = (val) => {
-  if (val === " ") return false;
+  if (val === " " || val === "\n") return false;
   return !isNaN(val);
 };
+
+var get_type = (val) => {
+  switch (val) {
+    case "U0":
+      return tokenType.u0;
+    case "I8":
+      return tokenType.i8;
+    case "U8":
+      return tokenType.u8;
+    case "I16":
+      return tokenType.i16;
+    case "U16":
+      return tokenType.u16;
+    case "I32":
+      return tokenType.i32;
+    case "U32":
+      return tokenType.u32;
+    case "I64":
+      return tokenType.i64;
+    case "U64":
+      return tokenType.u64;
+    case "F64":
+      return tokenType.f64;
+    default:
+      console.log("ERROR");
+  }
+};
+
+function holyc_lex_type(tokenList, line, input, i) {
+  if (input[i] === "U" || input[i] === "I" || input[i] === "F") {
+    if (input[i + 1] === "0" || input[i + 1] === "8") {
+      if (input[i + 1] === "0" && input[i] !== "U") {
+        return false;
+      }
+      aux = input[i++];
+      aux += input[i];
+      tokenList.push({
+        value: aux,
+        line: line,
+        type: get_type(aux),
+      });
+      return i;
+    } else if (
+      (input[i + 1] === "1" && input[i + 2] === "6") ||
+      (input[i + 1] === "3" && input[i + 2] === "2") ||
+      (input[i + 1] === "6" && input[i + 2] === "4")
+    ) {
+      if (
+        (input[i] === "F" && input[i + 1] !== "6") ||
+        (input[i] === "F" && input[i + 2] !== "4")
+      ) {
+        return false;
+      }
+      aux = input[i++];
+      aux += input[i++];
+      aux += input[i];
+      tokenList.push({
+        value: aux,
+        line: line,
+        type: get_type(aux),
+      });
+      return i;
+    } else {
+      return false;
+    }
+  }
+  return false;
+}
 
 function holyc_lex(input) {
   var tokenList = [];
   var line = 1;
 
   for (let i = 0; i < input.length; ++i) {
-    if (input[i] === " ") continue;
+    if (input[i] === "\n") line++;
+    if (input[i] === " " || input[i] === "\n") continue;
 
     if (input[i] === '"') {
       let aux = "";
+
       i++;
+
       while (input[i] !== '"') {
         aux += input[i++];
       }
@@ -64,21 +155,27 @@ function holyc_lex(input) {
     if (is_alpha(input[i])) {
       let aux = "";
 
+      if ((y = holyc_lex_type(tokenList, line, input, i))) {
+        i = y;
+        continue;
+      }
+
       while (is_alpha(input[i])) {
         aux += input[i++];
       }
+
+      i--;
 
       tokenList.push({
         value: aux,
         line: line,
         type: tokenType.id,
       });
+
+      continue;
     }
 
     switch (input[i]) {
-      case "\n":
-        line++;
-        break;
       case "+":
         tokenList.push({
           value: "+",
@@ -107,15 +204,35 @@ function holyc_lex(input) {
           type: tokenType.div,
         });
         break;
+      case ";":
+        tokenList.push({
+          value: ";",
+          line: line,
+          type: tokenType.semi,
+        });
+        break;
+      case "{":
+        tokenList.push({
+          value: "{",
+          line: line,
+          type: tokenType.rbrace,
+        });
+        break;
+      case "}":
+        tokenList.push({
+          value: "}",
+          line: line,
+          type: tokenType.lbrace,
+        });
+        break;
     }
   }
+  console.log(tokenList);
   return tokenList;
 }
 
-function holyc_parser(tokenList=[]) {
-  
-  console.log(tokenList.length);
-  console.log(tokenList.value);
+function holyc_parser(tokenList = []) {
+  console.log("not implemented yet");
 }
 
 document.getElementById("code").addEventListener("keydown", function (e) {
