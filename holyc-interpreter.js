1d4cf78bc18dfd3347c5246d8816f1084dc3d2a7
leozamboni <leonardonunes169@gmail.com>
2022-02-24 00:07:38 -0300

    update

--- holyc-interpreter.js
+++ holyc-interpreter.js
@@ -35,7 +35,15 @@ var tokenType = {
   i64: 18,
   u64: 19,
   f64: 20,
+  rparen: 21,
+  lparen: 22,
 };
+
+var globalIndexForTokenList;
+
+var clearOutput = () => {
+  document.getElementById("output").value = "HolyC Interpreter version 1.0.0\n";
+}
 
 var is_alpha = (val) => {
   if (val === " " || val === "\n") return false;
@@ -45,6 +53,48 @@ var is_alpha = (val) => {
 var is_digit = (val) => {
   if (val === " " || val === "\n") return false;
   return !isNaN(val);
+};
+
+var lexer_error = (val) => {
+  var output = document.getElementById("output");
+  output.value += "interpretation failure\n";
+  throw new Error((output.value += `lexer: '${val}' unexpected value\n`));
+};
+
+var remove_tabs = (val) => {
+  return val.replace(/\t/g, "");
+};
+
+var parser_error = (val) => {
+  var output = document.getElementById("output");
+  output.value += "interpretation failure\n";
+  throw new Error((output.value += `parser: '${val}' unexpected value\n`));
+};
+
+var list_eat = (token, expectedType) => {
+  if (token.type !== expectedType) {
+    parser_error(token.value);
+  }
+  globalIndexForTokenList++;
+};
+
+var list_eat_type = (token) => {
+  if (
+    token.type === tokenType.u0 ||
+    token.type === tokenType.i8 ||
+    token.type === tokenType.u8 ||
+    token.type === tokenType.i16 ||
+    token.type === tokenType.u16 ||
+    token.type === tokenType.i32 ||
+    token.type === tokenType.u32 ||
+    token.type === tokenType.i64 ||
+    token.type === tokenType.u64 ||
+    token.type === tokenType.f64
+  ) {
+    globalIndexForTokenList++;
+  } else {
+    parser_error(token.value);
+  }
 };
 
 var get_type = (val) => {
@@ -116,9 +166,10 @@ function holyc_lex_type(tokenList, line, input, i) {
 }
 
 function holyc_lex(input) {
+  input = remove_tabs(input);
+
   var tokenList = [];
   var line = 1;
-
   for (let i = 0; i < input.length; ++i) {
     if (input[i] === "\n") line++;
     if (input[i] === " " || input[i] === "\n") continue;
@@ -128,7 +179,7 @@ function holyc_lex(input) {
 
       i++;
 
-      while (input[i] !== '"') {
+      while (input[i] !== '"' && input[i]) {
         aux += input[i++];
       }
 
@@ -137,6 +188,8 @@ function holyc_lex(input) {
         line: line,
         type: tokenType.str,
       });
+
+      continue;
     }
 
     if (is_digit(input[i])) {
@@ -146,11 +199,15 @@ function holyc_lex(input) {
         aux += input[i++];
       }
 
+      i--;
+
       tokenList.push({
         value: aux,
         line: line,
         type: tokenType.const,
       });
+
+      continue;
     }
 
     if (is_alpha(input[i])) {
@@ -226,38 +283,46 @@ function holyc_lex(input) {
           type: tokenType.lbrace,
         });
         break;
+      case "(":
+        tokenList.push({
+          value: "(",
+          line: line,
+          type: tokenType.rparen,
+        });
+        break;
+      case ")":
+        tokenList.push({
+          value: ")",
+          line: line,
+          type: tokenType.lparen,
+        });
+        break;
+      default:
+        lexer_error(input[i]);
+        break;
     }
   }
-  console.log(tokenList);
+
   return tokenList;
 }
 
-var globalIndexForTokenList;
+function holyc_parser_parse_block(tokenList = []) {
+  if (tokenList[globalIndexForTokenList].type === tokenType.semi) return null;
 
-function list_eat(token, expectedType) {
-  if (token.type !== expectedType) {
-    throw new Error(`parser: ${token.value} unexpected value\n`);
-  }
-  globalIndexForTokenList++;
-}
+  var ast = new Ast();
 
-function list_eat_type(token) {
-  if (
-    token.type === tokenType.u0 ||
-    token.type === tokenType.i8 ||
-    token.type === tokenType.u8 ||
-    token.type === tokenType.i16 ||
-    token.type === tokenType.u16 ||
-    token.type === tokenType.i32 ||
-    token.type === tokenType.u32 ||
-    token.type === tokenType.i64 ||
-    token.type === tokenType.u64 ||
-    token.type === tokenType.f64
-  ) {
-    globalIndexForTokenList++;
-  } else {
-    throw new Error(`parser: ${token.value} unexpected value\n`);
+  switch (tokenList[globalIndexForTokenList].type) {
+    case tokenType.str:
+      ast.token = tokenList[globalIndexForTokenList];
+      list_eat(tokenList[globalIndexForTokenList], tokenType.str);
+      break;
+    default:
+      parser_error(tokenList[globalIndexForTokenList].value);
   }
+
+  ast.right = holyc_parser_parse_block(tokenList);
+
+  return ast;
 }
 
 function holyc_parser_parse_procedure(tokenList = []) {
@@ -269,17 +334,19 @@ function holyc_parser_parse_procedure(tokenList = []) {
   ast.next.token = tokenList[globalIndexForTokenList];
   list_eat(tokenList[globalIndexForTokenList], tokenType.id);
 
+  ast.next.next = new Ast();
+  ast.next.next.token = tokenList[globalIndexForTokenList];
+  list_eat(tokenList[globalIndexForTokenList], tokenType.rparen);
+
+  ast.next.next.next = new Ast();
+  ast.next.next.next.token = tokenList[globalIndexForTokenList];
+  list_eat(tokenList[globalIndexForTokenList], tokenType.lparen);
+
   ast.left = new Ast();
   ast.left.token = tokenList[globalIndexForTokenList];
   list_eat(tokenList[globalIndexForTokenList], tokenType.rbrace);
 
-  if (tokenList[globalIndexForTokenList].type === tokenType.str) {
-    console.log(tokenList[globalIndexForTokenList].value);
-
-    ast.right = new Ast();
-    ast.right.token = tokenList[globalIndexForTokenList];
-    list_eat(tokenList[globalIndexForTokenList], tokenType.str);
-  }
+  ast.right = holyc_parser_parse_block(tokenList);
 
   return ast;
 }
@@ -326,3 +393,14 @@ document.getElementById("code").addEventListener("keydown", function (e) {
     this.selectionStart = this.selectionEnd = start + 1;
   }
 });
+
+function examples() {
+  switch (document.getElementById("examples").value) {
+    case "helloWorld":
+      document.getElementById("code").value =
+        'U0\nHello()\n{\n\t"Hello World\\n";\n}\n\nHello;';
+      break;
+    default:
+      document.getElementById("code").value = "";
+  }
+}
